63702162d56b2618e999f956feea18f0e1ccfdf3
Yith <yith@Atlantis.local>
2010-06-19 17:34:22 -0400

    Fixed add_werewolf_stats self.down method
    The self.down method had add_column instead of remove_column and wasn't deleting the werewolf ideologies from their database table.

--- db/migrate/20100617184354_add_werewolf_stats.rb
+++ db/migrate/20100617184354_add_werewolf_stats.rb
@@ -12,7 +12,7 @@ class AddWerewolfStats < ActiveRecord::Migration
     add_column :characters, "read_renown", :boolean, :default => false
     add_column :characters, "gifts", :text
     add_column :characters, "read_gifts", :boolean, :default => false
-    add_column :characters, "totem" :text
+    add_column :characters, "totem", :text
     add_column :characters, "read_totem", :boolean, :default => false
     Ideology.create(:name => "Blood Talons")
     Ideology.create(:name => "Bone Shadows")
@@ -22,19 +22,24 @@ class AddWerewolfStats < ActiveRecord::Migration
   end
 
   def self.down
-    add_column :characters, "read_totem", :boolean, :default => false
-    add_column :characters, "totem" :text
-    add_column :characters, "read_gifts", :boolean, :default => false
-    add_column :characters, "gifts", :text
-    add_column :characters, "read_renown", :boolean, :default => false
-    add_column :characters, "cunning", :integer, :default => 0
-    add_column :characters, "wisdom", :integer, :default => 0
-    add_column :characters, "honor", :integer, :default => 0
-    add_column :characters, "glory", :integer, :default => 0
-    add_column :characters, "purity", :integer, :default => 0
-    add_column :characters, "read_essence", :boolean, :default => false
-    add_column :characters, "essence", :integer, :default => 7
-    add_column :characters, "read_primal_urge", :boolean, :default => false
-    add_column :characters, "primal_urge", :integer, :default => 0
+    Ideology.find_by_name('Storm Lords').delete
+    Ideology.find_by_name('Iron Masters').delete
+    Ideology.find_by_name('Hunters in Darkness').delete
+    Ideology.find_by_name('Bone Shadows').delete
+    Ideology.find_by_name('Blood Talons').delete
+    remove_column :characters, 'read_totem', :boolean, :default => false
+    remove_column :characters, 'totem', :text
+    remove_column :characters, 'read_gifts', :boolean, :default => false
+    remove_column :characters, 'gifts', :text
+    remove_column :characters, 'read_renown', :boolean, :default => false
+    remove_column :characters, 'cunning', :integer, :default => 0
+    remove_column :characters, 'wisdom', :integer, :default => 0
+    remove_column :characters, 'honor', :integer, :default => 0
+    remove_column :characters, 'glory', :integer, :default => 0
+    remove_column :characters, 'purity', :integer, :default => 0
+    remove_column :characters, 'read_essence', :boolean, :default => false
+    remove_column :characters, 'essence', :integer, :default => 7
+    remove_column :characters, 'read_primal_urge', :boolean, :default => false
+    remove_column :characters, 'primal_urge', :integer, :default => 0
   end
 end
