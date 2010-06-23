a2fc82036673547b1a48efeb7c9cfb2e20f3fe7d
Alex Chvatal <m.chvatal@gmail.com>
2010-06-23 11:53:23 -0400

    Shows splat.clique_name when viewing ideology
    "Clique" was hard-coded into ideologies/show... but uses ideology.splat.clique_name now.  Also, added in the pure tribes and ghost wolves and set all the werewolf ideologies to have the correct splat_id

--- db/migrate/20100617184354_add_werewolf_stats.rb
+++ db/migrate/20100617184354_add_werewolf_stats.rb
@@ -1,5 +1,15 @@
 class AddWerewolfStats < ActiveRecord::Migration
   def self.up
+    Splat.create(:name => "Werewolf", :nature_name => "Auspice", :clique_name => "Pack", :ideology_name => "Tribe", :morality_name => "Harmony", :power_stat_name => "Primal Urge", :fuel_name => "Essence")
+    Ideology.create(:name => "Blood Talons", :splat_id => Splat.find_by_name("Werewolf").id)
+    Ideology.create(:name => "Bone Shadows", :splat_id => Splat.find_by_name("Werewolf").id)
+    Ideology.create(:name => "Hunters in Darkness", :splat_id => Splat.find_by_name("Werewolf").id)
+    Ideology.create(:name => "Iron Masters", :splat_id => Splat.find_by_name("Werewolf").id)
+    Ideology.create(:name => "Storm Lords", :splat_id => Splat.find_by_name("Werewolf").id)
+    Ideology.create(:name => "Ghost Wolf", :splat_id => Splat.find_by_name("Werewolf").id)
+    Ideology.create(:name => "Fire-Touched", :splat_id => Splat.find_by_name("Werewolf").id)
+    Ideology.create(:name => "Ivory Claws", :splat_id => Splat.find_by_name("Werewolf").id)
+    Ideology.create(:name => "Predator Kings", :splat_id => Splat.find_by_name("Werewolf").id)
     add_column :characters, "purity", :integer, :default => 0
     add_column :characters, "glory", :integer, :default => 0
     add_column :characters, "honor", :integer, :default => 0
@@ -7,27 +17,25 @@ class AddWerewolfStats < ActiveRecord::Migration
     add_column :characters, "cunning", :integer, :default => 0
     add_column :characters, "gifts", :text
     add_column :characters, "totem", :text
-    Ideology.create(:name => "Blood Talons")
-    Ideology.create(:name => "Bone Shadows")
-    Ideology.create(:name => "Hunters in Darkness")
-    Ideology.create(:name => "Iron Masters")
-    Ideology.create(:name => "Storm Lords")
-    Splat.create(:name => "Werewolf", :nature_name => "Auspice", :clique_name => "Pack", :ideology_name => "Tribe", :morality_name => "Harmony", :power_stat_name => "Primal Urge", :fuel_name => "Essence")
   end
 
   def self.down
+    remove_column :characters, "totem", :text
+    remove_column :characters, "gifts", :text
+    remove_column :characters, "cunning", :integer, :default => 0
+    remove_column :characters, "wisdom", :integer, :default => 0
+    remove_column :characters, "honor", :integer, :default => 0
+    remove_column :characters, "glory", :integer, :default => 0
+    remove_column :characters, "purity", :integer, :default => 0
+    Ideology.find_by_name("Predator Kings").delete
+    Ideology.find_by_name("Ivory Claws").delete
+    Ideology.find_by_name("Fire-Touched").delete
+    Ideology.find_by_name("Ghost Wolf").delete
+    Ideology.find_by_name("Storm Lords").delete
+    Ideology.find_by_name("Iron Masters").delete
+    Ideology.find_by_name("Hunters in Darkness").delete
+    Ideology.find_by_name("Bone Shadows").delete
+    Ideology.find_by_name("Blood Talons").delete
     Splat.find_by_name("Werewolf").delete
-    Ideology.find_by_name('Storm Lords').delete
-    Ideology.find_by_name('Iron Masters').delete
-    Ideology.find_by_name('Hunters in Darkness').delete
-    Ideology.find_by_name('Bone Shadows').delete
-    Ideology.find_by_name('Blood Talons').delete
-    remove_column :characters, 'totem', :text
-    remove_column :characters, 'gifts', :text
-    remove_column :characters, 'cunning', :integer, :default => 0
-    remove_column :characters, 'wisdom', :integer, :default => 0
-    remove_column :characters, 'honor', :integer, :default => 0
-    remove_column :characters, 'glory', :integer, :default => 0
-    remove_column :characters, 'purity', :integer, :default => 0
   end
 end
