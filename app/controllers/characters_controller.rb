67956c4aa09779c88daefb17db71b9f1c9056bb4
Alex Chvatal <m.chvatal@gmail.com>
2010-06-25 15:41:00 -0400

    Passes a character object to update_splat

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -48,6 +48,8 @@ class CharactersController < ApplicationController
   end
   
   def update_splat
+    @character = Character.find(params[:char_id])
+
     respond_to do |format|
       format.js
     end
