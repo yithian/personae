48f4930fa942d3e104a7c6198e55f3e21f76c80c
Alex Chvatal <m.chvatal@gmail.com>
2010-10-03 23:23:25 -0400

    Adds some initial functional tests
    These tests probably need to be expanded upon to test as
    the storyteller, an owning user and a non-owning user

--- test/functional/cliques_controller_test.rb
+++ test/functional/cliques_controller_test.rb
@@ -1,41 +1,70 @@
 require 'test_helper'
 
 class CliquesControllerTest < ActionController::TestCase
-  test "should get index" do
+  def st_setup
+    User.create(:name => "Storyteller", :password => "pword")
+  end
+
+  def login_as(user)
+    @request.session[:user_id] = user ? user.id : nil
+  end
+  
+  test "should get index as user" do
+    st_setup
+    login_as(users(:one))
+    
     get :index
     assert_response :success
     assert_not_nil assigns(:cliques)
   end
 
-  test "should get new" do
+  test "should get new as user" do
+    st_setup
+    login_as(users(:one))
+    
     get :new
     assert_response :success
   end
 
-  test "should create clique" do
+  test "should create clique as user" do
+    st_setup
+    login_as(users(:one))
+    
     assert_difference('Clique.count') do
-      post :create, :clique => { }
+      post :create, :clique => { :name => "unique" }
     end
 
     assert_redirected_to clique_path(assigns(:clique))
   end
 
-  test "should show clique" do
+  test "should show as correct clique" do
+    st_setup
+    login_as(users(:one))
+    
     get :show, :id => cliques(:one).to_param
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit as correct user" do
+    st_setup
+    login_as(users(:one))
+    
     get :edit, :id => cliques(:one).to_param
     assert_response :success
   end
 
-  test "should update clique" do
+  test "should update clique as correct user" do
+    st_setup
+    login_as(users(:one))
+    
     put :update, :id => cliques(:one).to_param, :clique => { }
     assert_redirected_to clique_path(assigns(:clique))
   end
 
-  test "should destroy clique" do
+  test "should destroy clique as storyteller" do
+    st_setup
+    login_as(User.find_by_name("Storyteller"))
+    
     assert_difference('Clique.count', -1) do
       delete :destroy, :id => cliques(:one).to_param
     end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,45 +1,97 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
+  def st_setup
+    User.create(:name => "Storyteller", :password => "pword")
   end
-
+  
+  def login_as(user)
+    @request.session[:user_id] = user ? user.id : nil
+  end
+  
   test "should get new" do
+    st_setup
+    
     get :new
     assert_response :success
   end
 
   test "should create user" do
+    st_setup
+    
     assert_difference('User.count') do
-      post :create, :user => { }
+      post :create, :user => {:name => "unique", :password => "pword" }
     end
 
     assert_redirected_to user_path(assigns(:user))
   end
 
   test "should show user" do
+    st_setup
+    login_as(users(:one))
+    
     get :show, :id => users(:one).to_param
     assert_response :success
   end
+  
+  test "shouldn't show user" do
+    st_setup
+    
+    get :show, :id => users(:one).to_param
+    assert_response :redirect, @response.body
+  end
 
   test "should get edit" do
+    st_setup
+    
+    login_as(users(:one))
+    
     get :edit, :id => users(:one).to_param
     assert_response :success
   end
+  
+  test "shouldn't get edit" do
+    st_setup
+    
+    get :edit, :id => users(:one).to_param
+    assert_response :redirect
+  end
 
   test "should update user" do
+    st_setup
+    login_as(users(:one))
+    
     put :update, :id => users(:one).to_param, :user => { }
-    assert_redirected_to user_path(assigns(:user))
+    assert_response :success
+  end
+  
+  test "shouldn't update another user" do
+    st_setup
+    login_as(users(:two))
+    
+    put :update, :id => users(:one).to_param, :user => { }
+    assert_redirected_to user_path(users(:two))
   end
 
   test "should destroy user" do
+    st_setup
+    login_as(users(:one))
+    
     assert_difference('User.count', -1) do
       delete :destroy, :id => users(:one).to_param
     end
 
-    assert_redirected_to users_path
+    assert_redirected_to :controller => :characters
+  end
+  
+  test "shouldn't destroy another user" do
+    st_setup
+    login_as(users(:two))
+    
+    assert_no_difference('User.count') do
+      delete :destroy, :id => users(:one).to_param
+    end
+
+    assert_redirected_to user_path(users(:two))
   end
 end
