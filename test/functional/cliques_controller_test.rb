72c79fe164b1375dfb1bb42ae45a526171c07c22
Alex Chvatal <m.chvatal@gmail.com>
2010-10-04 16:44:00 -0400

    Fully fleshes out clique functional tests
    Some of these changes should be pushed to users functional tests
    as well (assert_equal for flash[:alert], for example)

--- test/functional/cliques_controller_test.rb
+++ test/functional/cliques_controller_test.rb
@@ -18,12 +18,26 @@ class CliquesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:cliques)
   end
 
+  test "shouldn't get index when not logged in" do
+    st_setup
+    
+    get :index
+    assert_response :redirect
+  end
+
   test "should get new as user" do
     st_setup
     login_as(users(:one))
     
     get :new
     assert_response :success
+  end
+
+  test "shouldn't get new when not logged in" do
+    st_setup
+    
+    get :new
+    assert_response :redirect
   end
 
   test "should create clique as user" do
@@ -35,9 +49,28 @@ class CliquesControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to clique_path(assigns(:clique))
+    assert_equal "Clique was successfully created.", flash[:notice], "character wasn't updated"
   end
 
-  test "should show as correct clique" do
+  test "shouldn't create clique when not logged in" do
+    st_setup
+    
+    assert_no_difference('Clique.count') do
+      post :create, :clique => { :name => "unique" }
+    end
+
+    assert_response :redirect
+  end
+
+  test "should show clique as storyteller" do
+    st_setup
+    login_as(User.find_by_name("Storyteller"))
+    
+    get :show, :id => cliques(:one).to_param
+    assert_response :success
+  end
+
+  test "should show clique as owning user" do
     st_setup
     login_as(users(:one))
     
@@ -45,20 +78,90 @@ class CliquesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit as correct user" do
+  test "shouldn't show clique as other user" do
     st_setup
-    login_as(users(:one))
+    login_as(users(:two))
+    
+    # clique two has write set to false
+    get :show, :id => cliques(:two).to_param
+    assert_response :redirect
+    assert_equal "You don't have permission to do that", flash[:notice], "character was updated"
+  end
+
+  test "shouldn't show clique when not logged in" do
+    st_setup
+    
+    get :show, :id => cliques(:one).to_param
+    assert_response :redirect
+  end
+
+  test "should get edit as storyteller" do
+    st_setup
+    login_as(User.find_by_name("Storyteller"))
     
     get :edit, :id => cliques(:one).to_param
     assert_response :success
   end
 
-  test "should update clique as correct user" do
+  test "should get edit as owning user" do
+    st_setup
+    login_as(users(:one))
+
+    cliques(:one).user_id = users(:one).id
+    cliques(:one).save
+    
+    get :edit, :id => cliques(:one).to_param
+    assert_response :success
+  end
+
+  test "shouldn't get edit as non-owning user" do
     st_setup
     login_as(users(:one))
     
+    get :edit, :id => cliques(:two).to_param
+    assert_response :redirect
+    assert_equal "You don't have permission to do that", flash[:notice], "character was updated"
+  end
+
+  test "shouldn't get edit when not logged in" do
+    st_setup
+    
+    get :edit, :id => cliques(:two).to_param
+    assert_response :redirect
+  end
+
+  test "should update clique as storyteller" do
+    st_setup
+    login_as(User.find_by_name("Storyteller"))
+    
     put :update, :id => cliques(:one).to_param, :clique => { }
     assert_redirected_to clique_path(assigns(:clique))
+    assert_equal "Clique was successfully updated.", flash[:notice], "character wasn't updated"
+  end
+
+  test "should update clique as owning user" do
+    st_setup
+    login_as(users(:one))
+
+    cliques(:one).user_id = users(:one).id
+    cliques(:one).save
+    
+    put :update, :id => cliques(:one).to_param, :clique => { }
+    assert_redirected_to clique_path(assigns(:clique))
+    assert_equal "Clique was successfully updated.", flash[:notice], "character wasn't updated"
+  end
+
+  test "shouldn't update clique as non-owning user" do
+    st_setup
+    login_as(users(:two))
+    
+    cliques(:one).user_id = users(:one).id
+    cliques(:one).save
+    
+    put :update, :id => cliques(:one).to_param, :clique => { }
+    
+    assert_redirected_to :controller => :cliques
+    assert_equal "You don't have permission to do that", flash[:notice], "character was updated"
   end
 
   test "should destroy clique as storyteller" do
@@ -70,5 +173,46 @@ class CliquesControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to cliques_path
+  end
+
+  test "should destroy clique as owning user" do
+    st_setup
+    login_as(users(:one))
+    
+    cliques(:one).user_id = users(:one).id
+    cliques(:one).save
+
+    assert_difference('Clique.count', -1) do
+      delete :destroy, :id => cliques(:one).to_param
+    end
+
+    assert_redirected_to cliques_path
+  end
+
+  test "shouldn't destroy clique as non-owning user" do
+    st_setup
+    login_as(users(:two))
+    
+    cliques(:one).user_id = users(:one).id
+    cliques(:one).save
+
+    assert_no_difference('Clique.count', -1) do
+      delete :destroy, :id => cliques(:one).to_param
+    end
+
+    assert_redirected_to cliques_path
+  end
+
+  test "shouldn't destroy clique when not logged in" do
+    st_setup
+    
+    cliques(:one).user_id = users(:one).id
+    cliques(:one).save
+
+    assert_no_difference('Clique.count', -1) do
+      delete :destroy, :id => cliques(:one).to_param
+    end
+
+    assert_response :redirect
   end
 end
