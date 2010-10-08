29eeeca6d3126b04d01465657156cd83393dc7ee
Alex Chvatal <m.chvatal@gmail.com>
2010-10-08 12:21:51 -0400

    Moves ideology permissions logic into model

--- app/controllers/ideologies_controller.rb
+++ app/controllers/ideologies_controller.rb
@@ -1,6 +1,6 @@
 class IdeologiesController < ApplicationController
   respond_to :html, :xml
-  before_filter :find_ideology, :only => [:show, :edit, :update, :destroy]
+  before_filter :find_ideology, :only => [:new, :show, :edit, :update, :destroy]
   before_filter :permission, :only => [:new, :create, :edit, :update, :destroy]
   before_filter :show_permission, :only => [:show]
   
@@ -22,8 +22,6 @@ class IdeologiesController < ApplicationController
   # GET /ideologies/new
   # GET /ideologies/new.xml
   def new
-    @ideology = Ideology.new
-
     respond_with @ideology
   end
 
@@ -71,26 +69,24 @@ class IdeologiesController < ApplicationController
   
   private
   def find_ideology
-    @ideology = Ideology.find(params[:id])
+    if params[:action] == "new"
+      @ideology = Ideology.new
+    else
+      @ideology = Ideology.find_by_id(params[:id])
+    end
   end
   
   def permission
     unless session[:user_id] == User.find_by_name("Storyteller").id
       flash[:notice] = "You don't have permission to do that"
-      redirect_to :action => :index
+      redirect_to :action => "index"
     end
   end
   
   def show_permission
-    known_ideology = false
-    known_ideology = true if session[:user_id] == User.find_by_name('Storyteller').id
-    @ideology.characters.each do |member|
-      known_ideology = true if member.read_ideology
-    end
-    
-    unless known_ideology
+    unless @ideology.is_known_to_user?(session[:user_id])
       flash[:notice] = "You don't have permission to do that"
-      redirect_to :action => :index
+      redirect_to :action => "index"
     end
   end
 end
--- app/models/ideology.rb
+++ app/models/ideology.rb
@@ -4,4 +4,18 @@ class Ideology < ActiveRecord::Base
 
   validates :name, :presence => true, :uniqueness => true
   validates :splat_id, :presence => true
+  
+  def is_known_to_user?(current_uid)
+    known_ideology = false
+    known_ideology = true if current_uid == User.find_by_name('Storyteller').id
+    self.characters.each do |member|
+      known_ideology = true if member.read_ideology
+    end
+
+    known_ideology
+  end
+
+  def can_destroy_as_user?(current_uid)
+    current_uid == User.find_by_name("Storyteller").id
+  end
 end
