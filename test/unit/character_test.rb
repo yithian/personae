94414f268d363ba925a33de68d9ca974eaae3475
Alex Chvatal <m.chvatal@gmail.com>
2010-10-09 17:10:20 -0400

    Removes unneeded character unit tests
    The schema has been updated to provide further defaults that no
    longer need to be checked for

--- test/unit/character_test.rb
+++ test/unit/character_test.rb
@@ -11,31 +11,13 @@ class CharacterTest < ActiveSupport::TestCase
     Character.create(:name => "Unique", :clique_id => 1, :virtue => "Charity", :vice => "Envy")
     c = Character.new(:name => "Unique", :clique_id => 1, :virtue => "Charity", :vice => "Envy")
     
-    assert(!c.save, "saved with a nono-unique name")
-  end
-  
-  test "shouldn't save without a clique id" do
-    c = Character.new(:name => "Unique", :virtue => "Charity", :vice => "Envy")
-    
-    assert(!c.save, "saved without a clique id")
-  end
-  
-  test "shouldn't save without a virtue" do
-    c = Character.new(:name => "Unique", :clique_id => 1, :vice => "Envy")
-    
-    assert(!c.save, "saved without a virtue")
+    assert(!c.save, "saved with a non-unique name")
   end
 
   test "shouldn't save with an invalid virtue" do
     c = Character.new(:name => "Unique", :clique_id => 1, :virtue => "Beauty", :vice => "Envy")
 
     assert(!c.save, "saved with an invalid virtue")
-  end
-  
-  test "shouldn't save without a vice" do
-    c = Character.new(:name => "Unique", :clique_id => 1, :virtue => "Charity")
-    
-    assert(!c.save, "saved without a vice")
   end
 
   test "shouldn't save with an invalid vice" do
