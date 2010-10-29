21b30495ba45ce9b2548793a7a234169a52043a8
Alex Chvatal <m.chvatal@gmail.com>
2010-10-29 19:21:58 -0400

    Adds test for password reset email

--- test/functional/user_mailer_test.rb
+++ test/functional/user_mailer_test.rb
@@ -8,4 +8,13 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal([users(:one).email_address], email.to)
     assert_equal("Forgotten username", email.subject)
   end
+  
+  test "should send reset password link" do
+    users(:one).generate_reset_code
+    email = UserMailer.forgot_password(users(:one), "http://some.hostname.tld/admin/reset_password?reset_code=#{users(:one).reset_code}").deliver
+    
+    assert(!ActionMailer::Base.deliveries.empty?, "message didn't get queued")
+    assert_equal([users(:one).email_address], email.to)
+    assert_equal("Reset password", email.subject)
+  end
 end
