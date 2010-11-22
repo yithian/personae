5b7251b5784ee1b047b12b3792f7a9f15285da66
Alex Chvatal <m.chvatal@gmail.com>
2010-11-22 17:55:21 -0500

    Adds unit tests for chronicle

--- test/unit/chronicle_test.rb
+++ test/unit/chronicle_test.rb
@@ -2,7 +2,15 @@ require 'test_helper'
 
 class ChronicleTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "shouldn't save without a name" do
+    c = Chronicle.new
+    assert(!c.save, "Saved without a name.")
+  end
+  
+  test "shouldn't save with a non-unique name" do
+    Chronicle.create(:name => "unique")
+    c = Chronicle.new(:name => "unique")
+    
+    assert(!c.save, "Saved with a non-unique name")
   end
 end
