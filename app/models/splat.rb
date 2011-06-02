964e8d07afc4bf655de1083fa369c1e3beb1c785
Alex Chvatal <m.chvatal@gmail.com>
2011-06-02 14:43:15 -0400

    Adds documentation for the splat model

--- app/models/splat.rb
+++ app/models/splat.rb
@@ -1,3 +1,12 @@
+# Represents a splat, which is to say a supernatural template
+# in the World of Darkness. While technically not a splat,
+# mortals have a splat entry as well for compatability reasons.
+# 
+# Splat is basically a way to adjust the terminology used for
+# the different templates. Paths are meaningless to Vampires,
+# for instance, but Paths and Clans are similar enough in their
+# function that they can be lumped together as Nature.
+
 class Splat < ActiveRecord::Base
   has_many :characters
   has_many :ideologies
