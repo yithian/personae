a6f6bd95dc9db8e2e3e0a895a46aae3c8ebab801
Alex Chvatal <m.chvatal@gmail.com>
2011-06-02 14:16:15 -0400

    Ensures known cliques are from known characters
    Previously, known cliques would be from any clique with a member
    that had read_clique set. Now, the user must be able to see
    that user's clique for it to show up.

--- app/models/clique.rb
+++ app/models/clique.rb
@@ -18,8 +18,9 @@ class Clique < ActiveRecord::Base
   def is_known_to_user?(user_id)
     known_clique = false
     known_clique = true if user_id == User.find_by_name("Storyteller").id or self.user_id == user_id or self.write or self.id == Clique.find_by_name("Solitary").id
-    self.characters.each do |member|                                                                                                                        
-      known_clique = true if member.read_clique                                                                                                                
+
+    self.characters.each do |member|
+      known_clique = true if member.read_clique and member.show_clique_to_user?(user_id)
     end
 
     known_clique
