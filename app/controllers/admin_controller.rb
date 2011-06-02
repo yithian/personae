7016513ac6f78857dd86fb45fd6f5dc33fb94fd2
Alex Chvatal <m.chvatal@gmail.com>
2011-06-02 15:25:31 -0400

    Adds documentation for admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,8 @@
+# Controller for administrative tasks: logging in/out, etc
+
 class AdminController < ApplicationController  
+  # Logs a user in with a given username and password, then
+  # redirects them to their original destination.
   def login
     if request.post?
       user = User.authenticate(params[:name], params[:password])
@@ -13,6 +17,7 @@ class AdminController < ApplicationController
     end
   end
 
+  # Logs a user out by removing the user_id from their session.
   def logout
     session[:user_id] = :logged_out
     flash[:notice] = "Logged out"
@@ -20,6 +25,8 @@ class AdminController < ApplicationController
     redirect_to :controller => :admin, :action => :login
   end
   
+  # Sends an email to the email address associated with a user containing
+  # the user's username.
   def forgot_username
     if request.post?
       user = User.find_by_email_address(params[:email_address])
@@ -30,6 +37,9 @@ class AdminController < ApplicationController
     end
   end
  
+  # Generates a reset code and emails it to the email address associated with
+  # the user's username. This allows them to reset their password on a
+  # single-use basis.
   def forgot_password
     if request.post?
       user = User.find_by_name(params[:username])
@@ -41,6 +51,7 @@ class AdminController < ApplicationController
     end
   end
 
+  # Changes a user's password with a given reset code.
   def reset_password
     @reset_code = params[:reset_code]
     @user = User.find_by_reset_code(@reset_code)
