7ee1be4035f898f3042c23f9de26da74f82e2b1e
Alex Chvatal <m.chvatal@gmail.com>
2011-06-02 14:25:38 -0400

    Adds documentation for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
+# Implements basic commenting support
+
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :character
@@ -6,6 +8,7 @@ class Comment < ActiveRecord::Base
   validates :user_id, :presence => true
   validates :body, :presence => true
   
+  # Returns true if the logged in user can edit the comment.
   def can_edit_as_user?(user_id)
     self.user_id == user_id or user_id == User.find_by_name("Storyteller").id
   end
