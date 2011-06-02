51e0b689c07a1c380990cb4ba679c07a6bfbbffe
Alex Chvatal <m.chvatal@gmail.com>
2011-06-02 14:37:20 -0400

    Ensures known ideologies are from known characters
    Previously, known ideologies would be from any character with
    read_ideology set to true. Now, it will only be for known
    characters with read_ideology set.

--- app/models/ideology.rb
+++ app/models/ideology.rb
@@ -5,11 +5,12 @@ class Ideology < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   validates :splat_id, :presence => true
   
+  # Returns true if the logged in user can see any characters with this ideology.
   def is_known_to_user?(user_id)
     known_ideology = false
     known_ideology = true if user_id == User.find_by_name('Storyteller').id
     self.characters.each do |member|
-      known_ideology = true if member.read_ideology
+      known_ideology = true if member.read_ideology and member.show_ideology_to_user?(user_id)
     end
     User.find_by_id(user_id).characters.each do |character|
       known_ideology = true if self.id == character.ideology.id
