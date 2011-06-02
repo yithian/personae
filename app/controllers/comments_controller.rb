349d8130d7d4455fca0565c2f37cdd8f47cab161
Alex Chvatal <m.chvatal@gmail.com>
2011-06-02 16:34:08 -0400

    Adds documentation for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,19 @@
+# Controller for comment actions: create/edit/destroy
+
 class CommentsController < ApplicationController
   respond_to :html, :xml
   before_filter :find_character, :only => [:new, :create, :destroy]
   before_filter :find_comment, :only => ["new", "destroy"]
   before_filter :destroy_permission, :only => ["destroy"]
 
+  # GET /characters/comments/new
+  # GET /characters/comments/new.xml
   def new
     respond_with @comment
   end
 
+  # POST /characters/comments
+  # POST /characters/comments
   def create
     @comment = @character.comments.build(params[:comment])
 
@@ -18,6 +24,8 @@ class CommentsController < ApplicationController
     end
   end
 
+  # DELETE /characters/comments/1
+  # DELETE /characters/comments/1.xml
   def destroy
     @comment.destroy
 
@@ -28,6 +36,8 @@ class CommentsController < ApplicationController
   end
 
   private
+  # Sets up a character variable and allows or denies access to
+  # comment on it based on Character#show_name_to_user?
   def find_character
     @character = Character.find_by_id(params[:character_id])
     unless @character.show_name_to_user?(session[:user_id])
@@ -36,6 +46,8 @@ class CommentsController < ApplicationController
     end
   end
 
+  # Sets up a comment object based on an id passed by url or
+  # creates a new (empty) comment.
   def find_comment
     if params[:action] == "new"
       @comment = @character.comments.build
@@ -44,6 +56,8 @@ class CommentsController < ApplicationController
     end
   end
 
+  # Allows or denies access to destroy a comment based on
+  # Comment#can_edit_as_user?
   def destroy_permission
     unless @comment.can_edit_as_user?(session[:user_id])
       flash[:notice] = "You don't have permission to do that"
