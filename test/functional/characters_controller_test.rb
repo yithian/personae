846f7d8dba3deee62abcffc13716f822d70e349a
Alex Chvatal <m.chvatal@gmail.com>
2011-06-23 14:03:56 -0400

    makes test work

--- test/functional/characters_controller_test.rb
+++ test/functional/characters_controller_test.rb
@@ -35,7 +35,7 @@ class CharactersControllerTest < ActionController::TestCase
     sign_in(users(:one))
 
     assert_difference('Character.count') do
-      post :create, :character => { :name => "unique", :splat_id => splats(:one).id, :chronicle_id => chronicles(:one).id, :subnature_id => subnatures(:one).id }
+      post :create, :character => { :name => "unique", :splat_id => splats(:one).id, :chronicle_id => chronicles(:one).id, :nature_id => subnatures(:one).id }
     end
 
     assert_redirected_to character_path(assigns(:character))
