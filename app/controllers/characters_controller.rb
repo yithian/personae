680bdc46a8615f923c77ab2db6d0f448ea4538a5
Alex Chvatal <m.chvatal@gmail.com>
2011-07-28 23:24:19 -0400

    syncs characters to obsidian portal on create

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -114,9 +114,18 @@ class CharactersController < ApplicationController
 
     flash[:notice] = 'Character was successfully created.' if @character.save
     
-    json_character = JSON.generate({:character => {:name => @character.name, :bio => @character.obsidian_bio, :description => @character.obsidian_description}})
-
-    obsidian_portal.access_token.post("/v1/campaigns/#{@character.chronicle.obsidian_campaign_id}/characters.json", json_character)
+    case @character.obsidian_character_id 
+    when 0
+      # do not sync
+    when -1
+      # create a new character
+      json_character = JSON.generate({:character => {:name => @character.name, :bio => @character.obsidian_bio, :description => @character.obsidian_description}})
+      obsidian_portal.access_token.post("/v1/campaigns/#{@character.chronicle.obsidian_campaign_id}/characters/#{@character.obsidian_character_id}.json", json_character)
+    else
+      # update existing character
+      json_character = JSON.generate({:character => {:name => @character.name, :bio => @character.obsidian_bio, :description => @character.obsidian_description}})
+      obsidian_portal.access_token.put("/v1/campaigns/#{@character.chronicle.obsidian_campaign_id}/characters/#{@character.obsidian_character_id}.json", json_character)
+    end
 
     respond_with @character
   end
