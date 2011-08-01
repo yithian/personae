31e0c17ba1cb55fd79a19eb3ae7dad0c0d559f6a
Alex Chvatal <m.chvatal@gmail.com>
2011-08-01 15:39:02 -0400

    allows non-admin users to connect to OP

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,11 +3,13 @@
 class ServicesController < ApplicationController
   include MageHand
   respond_to :html, :xml
-  load_and_authorize_resource :class => false
   before_filter :obsidian_portal_login_required, :only => [:obsidian_connect]
+  before_filter :logged_in?
 
   # POST /services/obsidian_connect
   def obsidian_connect
+    authorize! :obsidian, :connect
+
     user = current_user
     user.obsidian_user_id = obsidian_portal.current_user.id
     user.obsidian_user_name = obsidian_portal.current_user.username
@@ -19,11 +21,24 @@ class ServicesController < ApplicationController
 
   # POST /services/obsidian_disconnect
   def obsidian_disconnect
+    authorize! :obsidian, :disconnect
+
     user = current_user
     user.obsidian_user_id = nil
 
     user.save
 
     redirect_to root_path
+  end
+
+  protected
+  # since cancan is providing a temporary user to
+  # check permissions, it won't adequately check
+  # to see if a user needs to log in.
+  def logged_in?
+    unless user_signed_in?
+      flash[:alert] = "You need to sign in or sign up before continuing."
+      redirect_to new_user_session_path
+    end
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -22,6 +22,8 @@ class Ability
       can :manage, Chronicle, :owner_id => user.id
       can :manage, Comment, :user_id => user.id
       can :manage, User, :id => user.id
+      can :obsidian, :connect
+      can :obsidian, :disconnect
       can :read, :all
     end
     
