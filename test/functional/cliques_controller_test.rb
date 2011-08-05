81b1c072f40258002dac3a5146fe86c102e5164d
Alex Chvatal <m.chvatal@gmail.com>
2011-08-05 12:19:14 -0400

    adds additional view testing

--- test/functional/cliques_controller_test.rb
+++ test/functional/cliques_controller_test.rb
@@ -12,17 +12,49 @@ class CliquesControllerTest < ActionController::TestCase
     assert_redirected_to root_path
     assert_equal "Access denied!", flash[:error]
   end
+
+  def assert_index_view
+    # asserts the change_chronicle dropdown works
+    assert_select "select" do |elements|
+      elements.each do |element|
+        assert_select element, "option", {:minimum => 1}
+      end
+    end
+
+    assert_tag "td", :content => /MyOtherCliqueName/
+
+    xhr :get, :change_chronicle, :chronicle_id => chronicles(:one).id
+
+    assert_js_select "td", :content => /MyCliqueName/
+
+    # asserts that hidden cliques do not show
+    hidden_cliques = Clique.all - Clique.known_to(User.new, chronicles(:one).id)
+    hidden_cliques.each do |clique|
+      assert_no_tag "td", :content => /#{clique.name}/
+    end
+  end
+
+  def assert_show_view
+    assert_select "h3", :minimum => 2
+
+    assert_template "member"
+    
+    get :show, :id => cliques(:one).to_param
+    assert_select "h3", :content => /Totem/
+  end
   
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:cliques)
+    assert_index_view
 
     sign_in(users(:one))
     
     get :index
     assert_response :success
     assert_not_nil assigns(:cliques)
+    assert_index_view
   end
 
   test "should get new" do
@@ -63,6 +95,7 @@ class CliquesControllerTest < ActionController::TestCase
     get :show, :id => cliques(:one).to_param
     assert_response :success, @response.body
     assert_not_nil assigns(:clique)
+    assert_show_view
     
     # ST sees all cliques
     sign_in(users(:Storyteller))
@@ -74,6 +107,8 @@ class CliquesControllerTest < ActionController::TestCase
     assert_response :success, @response
     assert_not_nil assigns(:clique)
 
+    assert_show_view
+
     # show clique as member
     sign_in(users(:one))
 
@@ -81,12 +116,16 @@ class CliquesControllerTest < ActionController::TestCase
     assert_response :success, @response
     assert_not_nil assigns(:clique)
 
+    assert_show_view
+
     # show known clique as other user
     sign_in(users(:two))
 
     get :show, :id => cliques(:two).to_param
     assert_response :success, @repsonse
     assert_not_nil assigns(:clique)
+
+    assert_show_view
   end
 
   test "shouldn't show clique" do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,29 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # wrapper method around the native assert_select method that extracts out the html
+  # from raw html text that is embedded within javascript code.
+  # ex.
+  #   $('body').append("<div id=\"edit_post_form_container\" class=\"active modal_form_container\">
+  #     <a href=\"#\" class=\"close\">
+  #       <img alt=\"Close\" height=\"16\" src=\"/images/close.png?1293730609\" width=\"16\" />
+  #     <\/a>
+  #     <form accept-charset=\"UTF-8\" action=\"/posts/1023110335\" .../>")
+  #
+  # assert_js_select "input[type=?][name=?]", "text", "foo[bar]", :count => 1
+  # assert_js_select "textarea", "Some long text"
+  # assert_js_select "textarea[name=?]", "post_text", "Some long text"
+  # assert_js_select "textarea[name=?][class=?]", ["post_text", "css_class", "Some long text"
+  def assert_js_select(*args, &block)
+    extracted_text = @response.body.match(/(\<.*\>)/)[0].
+      gsub("\\n", "").gsub("\\t", "").gsub(/\\"/, '"').gsub("\\", '')
+    if extracted_text.present?
+      doc = HTML::Document.new(CGI::unescapeHTML(extracted_text)).root
+      args.insert(0, doc)
+      assert_select(*args, &block)
+    else
+      assert false, "Unable to extract any html from the js code."
+    end
+  end
 end
