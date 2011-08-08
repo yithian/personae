1a133bfc4d0f2ed3493fae9461f17c5893f9cd22
Alex Chvatal <m.chvatal@gmail.com>
2011-08-08 08:11:14 -0400

    spaces out dots into groups of 5
    this is not complete for shapeshifting

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,4 +1,9 @@
+# coding: utf-8
 # Character helper module
 
 module CharactersHelper
+  # breaks strings of •s into groups of 5 for readability
+  def dot_format(rating)
+    ("•" * rating).gsub(/•{5}/, "••••• ").strip
+  end
 end
