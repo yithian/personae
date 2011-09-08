3f3bd8f6e3e27aaac21c29a2e40cec146dd0b476
Joseph Henrich <crimsonknave@gmail.com>
2011-09-08 14:53:36 -0400

    Revert "Adding in the base elements of the modular design"
    This reverts commit 242e820fbb31ea4f74e2f8b27e26728072978ddc.
    
    Did not intend to commit to master (I'm awesome!)

--- app/models/blocks_blocks.rb
+++
@@ -1,4 +0,0 @@
-class BlocksBlocks < ActiveRecord::Base
-  belongs_to :inner_block, :foreign_key => :inner_block_id, :class_name => "Block"
-  belongs_to :outer_block, :foreign_key => :outer_block_id, :class_name => "Block"
-end
--- app/models/item.rb
+++
@@ -1,3 +0,0 @@
-class Item < ActiveRecord::Base
-  belongs_to :blocks
-end
