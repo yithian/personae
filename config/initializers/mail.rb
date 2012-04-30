d66b00d9bcf75e17164ab5a032f8cd0f8d5cc2e8
Alex Chvatal <m.chvatal@gmail.com>
2012-04-30 08:40:58 -0400

    use gmail as temporary smtp server
    until openshift gets its own outbound mail or provides custom
    environment variables, this uses a gmail account with the password
    set to the same as the database's for privacy reasons

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
+  :address        => 'smtp.gmail.com',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
+  :user_name      => 'infinitepersonae@gmail.com',
+  :password       => ENV['OPENSHIFT_DB_PASSWORD'],
+  :domain         => 'gmail.com'
 }
 ActionMailer::Base.delivery_method = :smtp
