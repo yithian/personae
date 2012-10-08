5e7c5e8b24b8d5812b408b4271dee44797f39927
Alex Chvatal <m.chvatal@gmail.com>
2012-10-07 22:26:32 -0400

    precompile bootstrap in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,7 @@ Personae::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += %w( bootstrap.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
