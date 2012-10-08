643ed64818d3f372ef30a176a47915e743d60e29
Alex Chvatal <achvatal@redhat.com>
2012-10-08 14:38:44 -0400

    update url change changing chronicle. closes #15

--- app/controllers/cliques_controller.rb
+++ app/controllers/cliques_controller.rb
@@ -23,7 +23,7 @@ class CliquesController < ApplicationController
   # POST /cliques/change_chronicle
   def change_chronicle
     # this bit of weirdness is to ensure the chronicle actually exists
-    @selected_chronicle.id = Chronicle.find_by_id(params[:new_chronicle_id])
+    @selected_chronicle = Chronicle.find_by_id(params[:new_chronicle_id])
 
     if user_signed_in?
       current_user.selected_chronicle = Chronicle.find_by_id(params[:chronicle_id])
