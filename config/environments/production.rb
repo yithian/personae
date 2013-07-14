bd9af2166a364acb1764b4bba4ac2ccd80d51580
Alex Chvatal <m.chvatal@gmail.com>
2013-07-14 09:52:57 -0400

    send valid links in emails!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Personae::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  # Url for mailer links
+  config.action_mailer.default_url_options = { :host => 'www.infinitepersonae.com' }
 end
