d7be27f6cc16c97ee740291e80d4e901be3c9c2b
Alex Chvatal <m.chvatal@gmail.com>
2013-07-23 20:21:21 -0400

    static functional tests pass

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,9 +2,9 @@
 # so it can use the rest of the site's layout
 class StaticController < ApplicationController
   skip_authorization_check
-  
+
   # GET index.html
   def index
-    @latest_chronicles = Chronicle.all(:order => "id DESC").last(10)
+    @latest_chronicles = Chronicle.order("id DESC").last(10)
   end
 end
