1c5c91e6b75979759b5c2bfe0aa54c02ad56e5df
Alex Chvatal <m.chvatal@gmail.com>
2013-07-23 21:16:27 -0400

    character functional tests pass

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,20 +4,20 @@ class CharactersController < ApplicationController
   include MageHand
   respond_to :html, :xml
   load_and_authorize_resource
-  
+
   before_filter :obsidian_portal_login_required, :only => [:new, :create, :edit, :update, :destroy], :if => :obsidian_enabled?
   before_filter :find_character, :only => [:new, :show, :shapeshift, :edit, :update, :save_notes, :save_current, :destroy, :preview]
   before_filter :set_params, :only => [:new]
   before_filter :find_lists, :only => [:new, :edit, :update]
-  
+
   skip_before_filter :chronicle_setup, :only => [:roll, :preview, :shapeshift, :possess, :update_splat, :update_nature, :update_chronicle]
-  
+
   # GET /characters
   # GET /characters.xml
   def index
     @pcs = @selected_chronicle.pcs.reject { |c| cannot? :read, c }
     @npcs = @selected_chronicle.find_npcs(current_user, params[:page])
-    
+
     respond_with @selected_chronicle, @pcs, @npcs
   end
 
@@ -26,39 +26,39 @@ class CharactersController < ApplicationController
   def show
     respond_with @character
   end
-  
+
   # POST /characters/1/roll
   def roll
     @dice_count = 0
     reroll = 10
     cancel = false
     @error = false
-    
+
     params[:dice_count].gsub( /(\d+)\s*-\s*(\d+)/, '\\1 + -\\2').split(/\s*\+\s*/).each { |d| @dice_count += d.to_i }
-    
+
     case params[:reroll]
     when '10', '9', '8'
       reroll = params[:reroll].to_i
     when 'rote'
       reroll = 0
     end
-    
+
     cancel = true if params[:cancel]
-    
+
     result = DiceRoller::DicePool.new(0, 0, 0, @dice_count).roll_pool
-    
+
     if @dice_count <= 50
       @successes = result.successes(8, reroll, cancel)
       @dice_results = result.ten_result
     else
       @error = true
     end
-    
+
     respond_to do |format|
       format.js
     end
   end
-  
+
   # GET /characters/preview
   def preview
     respond_to do |format|
@@ -70,7 +70,7 @@ class CharactersController < ApplicationController
   # GET /characters/new.xml
   def new
     obsidian_characters if obsidian_enabled?
-    
+
     respond_with @character
   end
 
@@ -79,28 +79,28 @@ class CharactersController < ApplicationController
     obsidian_characters if obsidian_enabled?
     @users = ::User.all
   end
-  
+
   # POST /characters/update_splat
   def update_splat
     @splat = Splat.find_by_id(params[:splat_id])
-    @nature_list = Nature.find_all_by_splat_id(@splat.id)
+    @nature_list = Nature.where(splat_id: @splat.id)
     @subnature_list = Subnature.list_for_nature(@nature_list.first).collect
-    @ideology_list = Ideology.find_all_by_splat_id(@splat.id)
+    @ideology_list = Ideology.where(splat_id: @splat.id)
 
     respond_to do |format|
       format.js
     end
   end
-  
+
   # GET /characters/possess
   def possess
     @possessed = params[:active]
-    
+
     respond_to do |format|
       format.js
     end
   end
-  
+
   # POST /characters/update_nature
   def update_nature
     @nature = Nature.find_by_id(params[:nature_id])
@@ -110,7 +110,7 @@ class CharactersController < ApplicationController
       format.js
     end
   end
-  
+
   # POST /characters/update_chronicle
   def update_chronicle
     @chronicle = Chronicle.find_by_id(params[:new_chronicle_id])
@@ -131,13 +131,13 @@ class CharactersController < ApplicationController
   # POST /characters
   # POST /characters.xml
   def create
-    @character = Character.new(params[:character])
+    @character = Character.new(character_params)
     @character.owner = current_user
     find_lists
 
     if @character.save
-      flash[:notice] = 'Character was successfully created.' 
-      
+      flash[:notice] = 'Character was successfully created.'
+
       obsidian_save if obsidian_enabled?
     end
 
@@ -151,24 +151,24 @@ class CharactersController < ApplicationController
   # if an obsidian_character_id is specified
   def update
     @character.vice = params[:character_vice].join(" ") if params[:character_vice]
-    
-    if @character.update_attributes(params[:character])
+
+    if @character.update_attributes(character_params)
       flash[:notice] = 'Character was successfully updated.'
-      
-      obsidian_save if obsidian_enabled?      
+
+      obsidian_save if obsidian_enabled?
     end
-    
+
     respond_with(@character.chronicle, @character)
   end
-  
+
   # PUT /characters/1/save_notes
-  # 
+  #
   # this will animate saving the notes on a character's page
   def save_notes
     @character.notes = params[:character][:notes]
     @successful = @character.save
   end
-  
+
   # PUT /characters/1/save_current
   #
   # this will only save the current_health and current_willpower
@@ -183,7 +183,7 @@ class CharactersController < ApplicationController
     @character.current_ka = params[:character][:current_ka] if params[:character][:current_ka]
     @character.current_ren = params[:character][:current_ren] if params[:character][:current_ren]
     @character.current_sheut = params[:character][:current_sheut] if params[:character][:current_sheut]
-    
+
     if @character.save
       head :success
     else
@@ -201,7 +201,7 @@ class CharactersController < ApplicationController
       format.xml  { head :ok }
     end
   end
-  
+
   private
   # Sets up a character object based on the id passed by url. If none is given,
   # creates a new (empty) object.
@@ -226,7 +226,7 @@ class CharactersController < ApplicationController
       @character.ideology_id = params['ideology_id']
     end
   end
-  
+
   # Restricts dropdowns to only include information appropriate to the character's
   # chronicle and splat.
   def find_lists
@@ -239,30 +239,30 @@ class CharactersController < ApplicationController
         [clique.name, clique.id]
       end
     end
-    
-    @nature_list = Nature.find_all_by_splat_id(@character.splat.id).collect
+
+    @nature_list = Nature.where(:splat_id => @character.splat.id).collect
     @subnature_list = Subnature.list_for_nature(@character.nature)
-    @ideology_list = Ideology.find_all_by_splat_id(@character.splat.id).collect
-    
+    @ideology_list = Ideology.where(:splat_id => @character.splat.id).collect
+
     @splat_list = Splat.all.collect
     @chronicle_list = Chronicle.all.collect
   end
-  
+
   # sets up a variable of obsidian portal character names and ids
   # for use in a select tag
   def obsidian_characters
-    unless @character.chronicle.obsidian_campaign_id == '0' or @character.chronicle.obsidian_campaign_id.nil? 
+    unless @character.chronicle.obsidian_campaign_id == '0' or @character.chronicle.obsidian_campaign_id.nil?
       @obsidian_characters = JSON.parse(obsidian_portal.access_token.get("/v1/campaigns/#{@character.chronicle.obsidian_campaign_id}/characters.json").body).collect { |c| [c["name"], c["id"]] }
       @obsidian_characters.insert(0, ["-", 0])
       @obsidian_characters << ["Create new", -1]
     end
   end
-  
+
   # saves a character to obsidian portal if appropriate
   def obsidian_save
     json_character = JSON.generate({:character => {:name => @character.name, :bio => @character.obsidian_bio, :description => @character.obsidian_description, :is_game_master_only => (not @character.read_name), :is_player_character => @character.pc, :tagline => @character.concept}})
-    
-    case @character.obsidian_character_id 
+
+    case @character.obsidian_character_id
     when "0"
       # do not sync
       return
@@ -273,5 +273,10 @@ class CharactersController < ApplicationController
       # update existing character
       result =  obsidian_portal.access_token.put("/v1/campaigns/#{@character.chronicle.obsidian_campaign_id}/characters/#{@character.obsidian_character_id}.json", json_character)
     end
+  end
+
+  # generate strong parameters
+  def character_params
+    params.require(:character).permit(:name, :virtue, :vice, :clique_id, :ideology_id, :description, :background, :intelligence, :strength, :presence, :wits, :dexterity, :manipulation, :resolve, :stamina, :composure, :academics, :athletics, :animal_ken, :computer, :brawl, :empathy, :crafts, :drive, :expression, :investigation, :firearms, :intimidation, :medicine, :larceny, :persuasion, :occult, :stealth, :socialize, :politics, :survival, :streetwise, :science, :weaponry, :subterfuge, :skill_specialties, :health, :willpower, :speed, :initiative, :defense, :armor, :morality, :derangements, :merits, :power_stat, :death, :fate, :forces, :life, :matter, :mind, :prime, :space, :spirit, :time, :equipment, :common_spells, :owner_id, :read_name, :read_description, :read_background, :read_crunch, :max_fuel, :experience, :read_experience, :read_clique, :read_ideology, :read_nature, :splat_id, :purity, :glory, :honor, :wisdom, :cunning, :gifts, :nature_id, :animalism, :auspex, :celerity, :dominate, :majesty, :nightmare, :protean, :obfuscate, :vigor, :covenant_disciplines, :size, :transmutations, :dream, :hearth, :mirror, :smoke, :artifice, :darkness, :elements, :fang_and_tooth, :stone, :vainglory, :fleeting_spring, :eternal_spring, :fleeting_summer, :eternal_summer, :fleeting_autumn, :eternal_autumn, :fleeting_winter, :eternal_winter, :goblin_contracts, :pledges, :boneyard, :caul, :curse, :marionette, :oracle, :rage, :shroud, :keys, :ceremonies, :deeds, :read_deeds, :current_health, :current_willpower, :notes, :read_notes, :chronicle_id, :subnature_id, :resilience, :current_fuel, :obsidian_character_id, :concept, :tactics, :pc, :possessed, :envy, :gluttony, :greed, :lust, :pride, :sloth, :wrath, :vestments, :primary_vice, :current_infernal_will, :power, :finesse, :resistance, :numina, :influences, :ab, :current_ab, :ba, :current_ba, :ka, :current_ka, :ren, :current_ren, :sheut, :current_sheut, :affinities, :utterances)
   end
 end
--- app/models/subnature.rb
+++ app/models/subnature.rb
@@ -6,15 +6,15 @@ class Subnature < ActiveRecord::Base
   has_many :characters
   belongs_to :splat
   belongs_to :nature
-  
+
   validates :name, :presence => true, :uniqueness => true
   validates :nature_id, :presence => true
   validates :splat_id, :presence => true
-  
+
   # List subnatures for a given nature
   def self.list_for_nature(nature)
-    subnatures = Subnature.find_all_by_nature_id_and_splat_id(0, nature.splat.id)
-    subnatures = subnatures + Subnature.find_all_by_nature_id(nature.id)
+    subnatures = Subnature.where(nature_id: 0, splat_id: nature.splat.id)
+    subnatures = subnatures + Subnature.where(nature_id: nature.id)
     subnatures.collect
   end
 end
