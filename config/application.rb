a5186c71bb80dbfd2052d3df256a948ef95330bf
Alex Chvatal <m.chvatal@gmail.com>
2013-07-24 22:04:16 -0400

    make sure to include bootstrap

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module Personae
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += %w( bootstrap.css )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
