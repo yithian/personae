75b889adf08c43d3c6279cf253878c9c8f6c7507
Alex Chvatal <achvatal@redhat.com>
2016-07-11 14:44:50 -0400

    get static controller test working in rails 5

--- test/functional/static_controller_test.rb
+++ test/functional/static_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class StaticControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
+  include Devise::Test::ControllerHelpers
   
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:selected_chronicles)
