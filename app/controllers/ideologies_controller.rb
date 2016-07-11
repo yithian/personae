e1d9204580278eb6952bc2933311d505a411e5c5
Alex Chvatal <achvatal@redhat.com>
2016-07-11 14:37:53 -0400

    clean up ideologies controller tests and get the it working in rails 5

--- app/controllers/ideologies_controller.rb
+++ app/controllers/ideologies_controller.rb
@@ -4,9 +4,9 @@ class IdeologiesController < ApplicationController
   respond_to :html, :xml
   load_and_authorize_resource
 
-  before_filter :find_ideology, :only => [:new, :show, :edit, :update, :destroy]
-  before_filter :set_params, :only => [:new]
-  before_filter :find_lists, :only => [:new, :create, :edit, :update]
+  before_action :find_ideology, :only => [:new, :show, :edit, :update, :destroy]
+  before_action :set_params, :only => [:new]
+  before_action :find_lists, :only => [:new, :create, :edit, :update]
 
   # GET /ideologies
   # GET /ideologies.xml
--- test/functional/ideologies_controller_test.rb
+++ test/functional/ideologies_controller_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
 class IdeologiesControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
+  include Devise::Test::ControllerHelpers
 
   def assert_login
     assert_redirected_to new_user_session_path
-    assert_equal "You need to sign in or sign up before continuing.", flash[:alert]
+    assert_equal 'You need to sign in or sign up before continuing.', flash[:alert]
   end
 
   def assert_denied
     assert_redirected_to root_path
-    assert_equal "Access denied!", flash[:error]
+    assert_equal 'Access denied!', flash[:error]
   end
 
-  test "should get index" do
+  test 'should get index' do
     # sign_in(users(:one))
 
     get :index
@@ -27,135 +27,135 @@ class IdeologiesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:ideologies)
   end
 
-  test "should get new" do
+  test 'should get new' do
     sign_in(users(:Storyteller))
 
-    get :new, :splat_id => splats(:one).id
+    get :new, params: {splat_id: splats(:one).id}
     assert_response :success
   end
 
-  test "shouldn't get new as nobody" do
+  test 'shouldn\'t get new as nobody' do
     # when not logged in
     get :new
     assert_login
   end
 
-  test "shouldn't get new as user" do
+  test 'shouldn\'t get new as user' do
     # or when logged in
     sign_in(users(:one))
 
     get :new
     assert_redirected_to ideologies_path
-    assert_equal "Access denied!", flash[:error], "got new as user"
+    assert_equal 'Access denied!', flash[:error], 'got new as user'
   end
 
-  test "should create ideology" do
+  test 'should create ideology' do
     sign_in(users(:Storyteller))
 
     assert_difference('Ideology.count') do
-      post :create, :ideology => { :name => "test" }
+      post :create, params: {ideology: { name: 'test' }}
     end
 
     assert_redirected_to ideology_path(assigns(:ideology))
   end
 
-  test "shouldn't create ideology as nobody" do
+  test 'shouldn\'t create ideology as nobody' do
     # not logged in
-    assert_no_difference('Ideology.count', "created when not logged in") do
-      post :create, :ideology => { :name => "test" }
+    assert_no_difference('Ideology.count', 'created when not logged in') do
+      post :create, params: {ideology: { name: 'test' }}
     end
     assert_login
   end
 
-  test "shouldn't create ideology as user" do
-    # shouldn't create as user
+  test 'shouldn\'t create ideology as user' do
+    # shouldn\'t create as user
     sign_in(users(:one))
 
     assert_no_difference('Ideology.count') do
-      post :create, :ideology => { :name => "test" }
+      post :create, params: {ideology: { name: 'test' }}
     end
     assert_redirected_to ideologies_path
-    assert_equal "Access denied!", flash[:error], "created ideology as user"
+    assert_equal 'Access denied!', flash[:error], 'created ideology as user'
   end
 
-  test "should show ideology" do
+  test 'should show ideology' do
     # when not logged in
-    get :show, :id => ideologies(:two).to_param
+    get :show, params: {id: ideologies(:two).to_param}
     assert_response :success, @response.body
 
     # should get ideology
     sign_in(users(:one))
 
-    get :show, :id => ideologies(:one).to_param
+    get :show, params: {id: ideologies(:one).to_param}
     assert_response :success, @response
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     sign_in(users(:Storyteller))
 
-    get :edit, :id => ideologies(:one).to_param
+    get :edit, params: {id: ideologies(:one).to_param}
     assert_response :success, @response
   end
 
-  test "shouldn't get edit as nobody" do
-    # shouldn't get edit when not logged in
-    get :edit, :id => ideologies(:one).to_param
+  test 'shouldn\'t get edit as nobody' do
+    # shouldn\'t get edit when not logged in
+    get :edit, params: {id: ideologies(:one).to_param}
     assert_login
   end
 
-  test "shouldn't get edit as user" do
-    # shouldn't get edit as user
+  test 'shouldn\'t get edit as user' do
+    # shouldn\'t get edit as user
     sign_in(users(:one))
 
-    get :edit, :id => ideologies(:one).to_param
+    get :edit, params: {id: ideologies(:one).to_param}
     assert_redirected_to ideology_path(ideologies(:one))
-    assert_equal "Access denied!", flash[:error], "got edit for ideology as user"
+    assert_equal 'Access denied!', flash[:error], 'got edit for ideology as user'
   end
 
-  test "should update ideology" do
+  test 'should update ideology' do
     sign_in(users(:Storyteller))
 
-    put :update, :id => ideologies(:one).to_param, :ideology => { :name => 'tested' }
+    put :update, params: {id: ideologies(:one).to_param, ideology: { name: 'tested' }}
     assert_redirected_to ideology_path(assigns(:ideology))
   end
 
-  test "shouldn't update ideology as nobody" do
-    put :update, :id => ideologies(:one).to_param, :ideology => { :name => 'tested' }
+  test 'shouldn\'t update ideology as nobody' do
+    put :update, params: {id: ideologies(:one).to_param, ideology: { name: 'tested' }}
     assert_login
   end
 
-  test "shouldn't update ideology as user" do
+  test 'shouldn\'t update ideology as user' do
     sign_in(users(:one))
-    put :update, :id => ideologies(:one).to_param, :ideology => { :name => 'tested' }
-    assert_equal("Access denied!", flash[:error], "updated ideology as user")
+    put :update, params: {id: ideologies(:one).to_param, ideology: { name: 'tested' }}
+    assert_equal('Access denied!', flash[:error], 'updated ideology as user')
     assert_redirected_to ideology_path(ideologies(:one))
   end
 
-  test "should destroy ideology" do
+  test 'should destroy ideology' do
     sign_in(users(:Storyteller))
 
     assert_difference('Ideology.count', -1) do
-      delete :destroy, :id => ideologies(:one).to_param
+      delete :destroy, params: {id: ideologies(:one).to_param}
     end
 
     assert_redirected_to ideologies_path
   end
 
-  test "shouldn't destroy ideology as nobody" do
-    assert_no_difference('Ideology.count', "got past authentication") do
-      delete :destroy, :id => ideologies(:one).to_param
+  test 'shouldn\'t destroy ideology as nobody' do
+    assert_no_difference('Ideology.count', 'got past authentication') do
+      delete :destroy, params: {id: ideologies(:one).to_param}
     end
 
     assert_login
   end
 
-  test "shouldn't destroy ideology as user" do
+  test 'shouldn\'t destroy ideology as user' do
     sign_in(users(:one))
 
-    assert_no_difference('Ideology.count', "user destroyed ideology") do
-      delete :destroy, :id => ideologies(:one).to_param
+    assert_no_difference('Ideology.count', 'user destroyed ideology') do
+      delete :destroy, params: {id: ideologies(:one).to_param}
     end
     assert_redirected_to ideology_path(ideologies(:one))
-    assert_equal "Access denied!", flash[:error]
+    assert_equal 'Access denied!', flash[:error]
   end
 end
